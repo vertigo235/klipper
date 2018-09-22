6ed0204d600617a5ea88286da61224a766a3c0e5
Arksine <arksine.code@gmail.com>
2018-09-22 11:18:22 -0400

    bed_mesh: extend BED_MESH_MAP functionality for octoprint plugin

--- klippy/extras/bed_mesh.py
+++ klippy/extras/bed_mesh.py
@@ -269,7 +269,12 @@ class BedMeshCalibrate:
             self.probed_z_table[y_position][x_position] = \
                 avg_z - z_offset
         if self.build_map:
-            outdict = {'z_probe_offsets:': self.probed_z_table}
+            params = self.probe_params
+            outdict = {
+                'min_point': (params['min_x'], params['min_y']),
+                'max_point': (params['max_x'], params['max_y']),
+                'xy_offset': offsets[:2],
+                'z_positions:': self.probed_z_table}
             self.gcode.respond(json.dumps(outdict))
         else:
             mesh = ZMesh(self.probe_params)
